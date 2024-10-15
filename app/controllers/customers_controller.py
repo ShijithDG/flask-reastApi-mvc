657061bbf1fe5e52b95e0b484afc6b95538be8d0
shijithDG <shijith.pk@datagrokr.com>
2024-10-15 21:16:32 +0530

    feat: get-customer retrieve customer details with error handling

--- app/controllers/customers_controller.py
+++ app/controllers/customers_controller.py
@@ -3,6 +3,7 @@
 from flask import Blueprint, request, jsonify
 from app.models import db
 from app.models.customer import Customer
+# from app.models.customer import CustomerSchema
 
 customer_bp = Blueprint('customer', __name__)
 
@@ -38,6 +39,7 @@ def add_customer():
 
 @customer_bp.route('/customer-update/<string:customer_id>', methods=['PUT'])
 def update_customer(customer_id):
+
     print('inside update_customer')
     try:
         # Get the customer from the database
@@ -67,3 +69,33 @@ def update_customer(customer_id):
     except Exception as e:
         db.session.rollback()  # Rollback in case of error
         return jsonify({"error": str(e)}), 500
+    
+
+@customer_bp.route('/get-customer/<string:customer_id>', methods=['GET'])
+def get_customer(customer_id):
+    customer_schema = Customer()
+    print('inside get-customer')
+    try:
+        customer = Customer.query.filter_by(customer_id=customer_id).first()
+        print('this is customer',customer)
+        print('Company Name:', customer.company_name)
+        customer_data = {
+            'customer_id': customer.customer_id,
+            'company_name': customer.company_name,
+            'contact_name': customer.contact_name,
+            'contact_title': customer.contact_title,
+            'address': customer.address,
+            'city': customer.city,
+            'region': customer.region,
+            'postal_code': customer.postal_code,
+            'country': customer.country,
+            'phone': customer.phone,
+            'fax': customer.fax
+        }
+        if not customer:
+            return jsonify({'error': 'Customer not found'}), 404
+
+        return jsonify(customer_data), 200
+
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500  # Internal server error
