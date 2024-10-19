f59c3c378b282feff9af4b27ddcecd600a218680
shijithDG <shijith.pk@datagrokr.com>
2024-10-19 17:15:25 +0530

    feat : completed order updating api

--- app/controllers/orders_controller.py
+++ app/controllers/orders_controller.py
@@ -39,3 +39,37 @@ def add_orders():
         db.session.rollback()
         print(f'error : {e}')
         return jsonify({"message":"server error "}), 500
+    
+@orders_bp.route('/update-orders/<string:order_id>', methods=['PUT'])
+def update_order(order_id):
+    try:
+        orders = Orders.query.get(order_id)
+        
+        if not orders:
+            return jsonify({"message":"order not found"})
+        
+        data = request.get_json()
+        
+        orders.order_id = data.get('order_id', orders.order_id),
+        orders.customer_id = data.get('customer_id', orders.customer_id),
+        orders.employee_id = data.get('employee_id', orders.employee_id),
+        orders.order_date = data.get('order_date', orders.order_date),
+        orders.required_date = data.get('required_date', orders.required_date),
+        orders.shipped_date = data.get('shipped_date', orders.shipped_date),
+        orders.ship_via = data.get('ship_via', orders.ship_via),
+        orders.freight = data.get('freight', orders.freight)
+        orders.ship_name = data.get('ship_name', orders.ship_name),
+        orders.ship_address = data.get('ship_address', orders.ship_address),
+        orders.ship_city = data.get('ship_city', orders.ship_city),
+        orders.ship_region = data.get('ship_region', orders.ship_region),
+        orders.ship_postal_code = data.get('ship_postal_code', orders.ship_postal_code),
+        orders.ship_country = data.get('ship_country', orders.ship_country),
+        
+        db.session.commit()
+        return jsonify({'message': 'order updated successfully'})
+    
+    except Exception as e:
+        
+        db.session.rollback()
+        print(f"error : {e}")
+        return jsonify({'message': 'server error'}), 500
