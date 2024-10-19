49ab1360742069ab88a8c5726f5d9445a4c15f2d
shijithDG <shijith.pk@datagrokr.com>
2024-10-19 14:26:14 +0530

    feat : added order schema

---
+++ app/models/orders.py
@@ -0,0 +1,25 @@
+from app.models import db
+
+
+class Orders(db.Model):
+    
+    __table__name = 'Orders'
+    
+    order_id = db.Column(db.SmallInteger, primary_key=True, autoincrement=True)
+    customer_id = db.Column(db.String(20))  
+    employee_id = db.Column(db.SmallInteger)
+    order_date = db.Column(db.Date)
+    required_date = db.Column(db.Date)
+    shipped_date = db.Column(db.Date)
+    ship_via = db.Column(db.SmallInteger) 
+    freight = db.Column(db.Float)
+    ship_name = db.Column(db.String(40))
+    ship_address = db.Column(db.String(60))
+    ship_city = db.Column(db.String(15))
+    ship_region = db.Column(db.String(15))
+    ship_postal_code = db.Column(db.String(10))
+    ship_country = db.Column(db.String(15))
+    
+    def __repr__(self) -> str:
+        return f'<Products {self.product_name}>'
+    
